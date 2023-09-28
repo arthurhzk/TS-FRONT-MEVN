fe964050c308cc58d819a30d1fbf3a4e8abcfcdc
Arthur Kunz <arthur.kunz@universo.univates.br>
2023-09-28 19:18:25 -0300

    title ok

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
+  <the-nav></the-nav>
   <router-view></router-view>
 </template>
 
 <script lang="ts">
+import TheNav from "@/components/TheNav.vue";
 export default {
   name: "App",
+  components: {
+    TheNav,
+  },
 };
 </script>
 
---
+++ src/components/TheNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav class="h-20 bg-white flex items-center">
+      <router-link to="/">
+        <h1 class="saira text-4xl text-gray-600">Kunz Shoes</h1>
+      </router-link>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheNav",
+};
+</script>
+
+<style scoped>
+.saira {
+  font-family: "Saira Stencil One", cursive;
+}
+</style>
