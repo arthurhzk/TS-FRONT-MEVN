11dc452b2cf050d6c835b0eb939a3046c8522c51
Arthur Kunz <arthur.kunz@universo.univates.br>
2023-10-27 13:22:41 -0300

    feat: started using formKit

--- src/views/ProductLogin.vue
+++ src/views/ProductLogin.vue
@@ -14,7 +14,7 @@
     <form class="flex flex-col gap-5 pt-7">
       <div>
         <input
-          class="rounded-md px-3 py-2 w-80 border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          class="rounded-md px-3 py-2 w-80 border-gray-400 focus:outline-none"
           type="text"
           name="login"
           placeholder="Login ou E-mail"
@@ -22,7 +22,7 @@
       </div>
       <div>
         <input
-          class="rounded-md px-3 py-2 w-80 border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          class="rounded-md px-3 py-2 w-80 border-gray-400 focus:outline-none"
           type="password"
           name="password"
           placeholder="Senha"
--- src/views/RegisterForm.vue
+++ src/views/RegisterForm.vue
@@ -1,31 +1,81 @@
 <template>
-  <div><h4 class="form-label">Sign up</h4></div>
+  <div class="bg-slate-200 flex flex-col items-center justify-center gap-2 p-2">
+    <h1 class="font-bold text-lg leading-22 uppercase text-gray-600">
+      Cadastre-se
+    </h1>
 
-  <FormKit type="form" submit-label="Registrar">
-    <FormKit
-      type="text"
-      name="name"
-      id="name"
-      validation="required|not:Admin"
-      label="Nome"
-      placeholder="“João da Silva”"
-    />
+    <FormKit type="form" submit-label="Registrar">
+      <FormKit
+        class="form-kit"
+        type="text"
+        name="name"
+        id="name"
+        validation="required|length:5,15|alphanumeric"
+        label="Nome"
+        placeholder="“João da Silva”"
+        autocomplete="off"
+      />
 
-    <FormKit
-      type="select"
-      label="Gênero"
-      name="gender"
-      id="gender"
-      placeholder="Digite seu gênero"
-      :options="['Masculino', 'Feminino', 'Outros']"
-    />
-    <FormKit
-      type="email"
-      name="email"
-      id="email"
-      validation="required|email"
-      label="E-mail"
-      placeholder=""
-    />
-  </FormKit>
+      <FormKit
+        type="select"
+        label="Gênero"
+        name="gender"
+        id="gender"
+        placeholder=""
+        :options="['Masculino', 'Feminino', 'Outros']"
+      />
+
+      <FormKit
+        type="email"
+        name="email"
+        id="email"
+        validation="required|email"
+        label="E-mail"
+        placeholder="joaodasilva@email.com"
+      />
+
+      <FormKit
+        type="password"
+        name="password"
+        label="Senha"
+        validation="required"
+        placeholder="No mínimo 6 caracteres"
+      />
+
+      <FormKit
+        type="password"
+        name="password_confirm"
+        id="password_confirm"
+        label="Confirmar senha"
+        validation-label="Confirmação de senha"
+      />
+
+      <FormKit
+        type="text"
+        name="phone"
+        label="Telefone"
+        placeholder="(XX) XXXX-XXXX"
+        :validation="[['required'], ['matches', /^\d{3}-\d{3}-\d{4}$/]]"
+        :validation-messages="{
+          matches: 'Phone number must be formatted: (XX) XXXX-XXXX',
+        }"
+      />
+    </FormKit>
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: "RegisterForm",
+};
+</script>
+
+<style scoped>
+.form-kit {
+  border-radius: 0.25rem;
+  padding: 0.5rem 0.75rem;
+  width: 80%;
+  border: 1px solid #cbd5e0;
+  outline: none;
+}
+</style>
